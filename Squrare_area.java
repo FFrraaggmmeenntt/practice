3c3c3c70037cfa47add25b3263540170b69939bd
FFrraaggmmeenntt <130890200+FFrraaggmmeenntt@users.noreply.github.com>
2023-04-23 18:32:36 +0900

    Add files via upload

---
+++ Squrare_area.java
@@ -0,0 +1,39 @@
+package area;
+
+
+import java.util.*;
+public class Squrare_area {
+    int width;  //밑변
+    int height; //높이
+    
+   
+   public void printInput(int width, int height) {
+  	 System.out.println("당신이 입력한 밑변은 "+width+" 높이는 "+height+"입니다.");
+   }
+   public void printArea(int width, int height){
+  	 System.out.println("넓이는 "+ (width*height) +" 입니다.");
+  	 
+   }
+
+	public static void main(String[] args) {
+		
+		Squrare_area area = new Squrare_area();
+		Scanner scanner_width = new Scanner(System.in);
+		Scanner scanner_height = new Scanner(System.in);
+
+        System.out.println("넓이를 구하고자 하는 밑변을 입력해주세요");
+        int user_input_width = scanner_width.nextInt();
+        
+        System.out.println("넓이를 구하고자 하는 높이를 입력해주세요");
+        int user_input_height = scanner_height.nextInt();
+        
+        area.printInput(user_input_width,user_input_height);
+        area.printArea(user_input_width, user_input_height);
+        
+	
+	    scanner_width.close();
+	    scanner_height.close();
+	
+	}
+
+}
